85ec849697c0fe8bf10c55a4678abcfdd7bf2425
TEONNA ZARAGOZA <teonna.zaragoza@gmail.com>
2019-03-08 11:10:02 -0800

    can see hi on page

---
+++ src/reducers/countries.js
@@ -0,0 +1,10 @@
+const intialState = {
+  name: 'name',
+  capital: 'capital',
+  lang: 'language',
+  flag: 'flag'
+};
+
+export default function reducer(state = intialState, action) {
+  return state;
+}
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import countries from './countries';
+import { combineReducers } from 'redux';
+
+export default combineReducers({
+  countries
+});
